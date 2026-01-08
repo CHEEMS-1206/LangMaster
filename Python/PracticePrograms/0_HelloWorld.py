4707256b597e97ff21f978f061f3b3ef6ab1f7aa
PRIYANSHU SINGH <sentysingh456@gmail.com>
2026-01-08 21:15:15 +0530

    Program to write Hello world in python, and general steps to setup and interpret python  code

---
+++ Python/PracticePrograms/0_HelloWorld.py
@@ -0,0 +1,13 @@
+print("Hello World !")
+
+"""
+--> RUNNING C++ PROGRAMS - 
+    1> Setup interpreter
+        - install latest python
+        - add python to PATH variable
+        - check installation by
+            python --version
+        
+    2> Running programs
+        - python fileName.py -> interprets the file and executes it line by line
+""" 
